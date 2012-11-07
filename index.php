631fa764eb5f89c8570108475ac8d113236f22d6
OpenShift guest <miller.emily10@gmail.com>
2012-11-07 18:55:31 -0500

    basic HTML for a two-column layout

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php include('inc-head.php') ?>
+<body>
+
+        <div id="container">
+            <header>
+                <h1>Site name</h1>
+            </header>
+            <nav></nav>
+            <article>
+                <h2>Article name</h2>
+                    <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
+                    <iframe width="100%" height="166" scrolling="no" frameborder="no" src="http://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F66187230&show_artwork=true"></iframe>
+           </article>
+            <aside>
+                <iframe width="420" height="315" src="http://www.youtube.com/embed/0Bmhjf0rKe8" frameborder="0" allowfullscreen></iframe>
+                <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+            </aside>
+            <footer></footer>
+        
+        </div>
+        
+        
+        
+        
+        
+        
+        
+        
+<?php include ('inc-footer.php') ?>
